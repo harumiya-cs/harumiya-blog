46a4b997427e4f168798c7be044e14e1d3800554
Sherzod <thisisotajonov@gmail.com>
2023-06-21 11:56:14 +0200

    make naming clear for blogs section

--- blog/post/views.py
+++ blog/post/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Post
 
 # Create your views here.
-def blogs(request):
-  blogs = Post.objects.filter(status=1).order_by('-created_at')
-  context = {'blogs': blogs}
-  return render(request, 'post/blogs.html', context)
+def posts(request):
+  posts = Post.objects.filter(status=1).order_by('-created_at')
+  context = {'posts': posts}
+  return render(request, 'post/all_posts.html', context)
 
-def blog(request, slug):
+def post(request, slug):
   context = {
-     'blog': Post.objects.get(slug=slug)
+     'post': Post.objects.get(slug=slug)
   }
-  return render(request, 'post/blog.html', context)
+  return render(request, 'post/post_content.html', context)
