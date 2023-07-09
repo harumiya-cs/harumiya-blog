5966e016a7acb778a2fa0386a91eaed450fe6625
Sherzod <thisisotajonov@gmail.com>
2023-07-09 18:52:27 +0500

    move project secret key to .env

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ex(e0(yg7b*sgtbonj=v^*32-$5wi5+5-x*2tr)c(4_ycoeul7'
+SECRET_KEY = config('PROJECT_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
